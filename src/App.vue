f6c6e00b3e366f2db2975d3508396bdad6b0472d
Isaiah Hunter <hunter.isaiah96@gmail.com>
2020-01-10 23:29:02 -0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-container class="pa-0" fluid>
+      <v-system-bar height="31" dark color="primary">
+        <v-row justify="center" no-gutters>
+          <v-col cols="7">
+            <a class="white--text" href="#">
+              <v-icon color="white">mdi-email</v-icon>thomasprusso@gmail.com
+              <v-icon class="ml-4" color="white">mdi-facebook</v-icon>
+              <v-icon class="ml-4" color="white">mdi-instagram</v-icon>
+            </a>
+          </v-col>
+        </v-row>
+      </v-system-bar>
+      <v-app-bar elevation="1">
+        <v-row justify="center" no-gutters>
+          <v-col cols="7"></v-col>
+        </v-row>
+      </v-app-bar>
+    </v-container>
+    <!-- <v-content></v-content> -->
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style src="@/assets/css/main.scss" lang="scss"></style>
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  }),
+};
+</script>
